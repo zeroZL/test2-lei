dec6ae363f3bf46968d1f79d0974e64b52b3b7c9
codingto <460868874@qq.com>
2015-04-29 18:08:56 +0800

    use class User

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 require '../vendor/autoload.php';
 use Illuminate\Support\User;
-$user=new User(['a'=>1,2,3,4,5,6]);
-echo $user->get("a");
+$user=new User(['a'=>1,2,3,4,'e'=>5,6]);
+echo $user->get("b");
 echo $user->get(1);
+var_dump($user->getAttributes());
+var_dump($user->toArray());
+var_dump($user->jsonSerialize());
+var_dump($user->toJson());
+var_dump($user->offsetExists('q'));
+var_dump($user->offsetGet('r'));
+var_dump($user->offsetSet('b','a'));
+var_dump($user->locla('abc'));
+var_dump($user->attributes);
+var_dump($user->attributes['r']='s');
